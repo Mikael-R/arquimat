c86706eab8cbc3d1856e7ee3e5d21176c51099c9
Mikael Rolim de Aquino <mikarg9@gmail.com>
2020-10-30 18:49:54 +0000

    feat: 🎸 add initial config for player status page

--- src/pages/PlayerStatus/index.tsx
+++ src/pages/PlayerStatus/index.tsx
@@ -1,10 +1,26 @@
 import React, { ReactElement } from 'react';
 
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
 function PlayerStatus(): ReactElement {
   return (
-    <div>
-      <h1>Infelismente este recurso ainda não foi concluído.</h1>
-      <h2>Mas estou trabalhando intensamente, volte em breve ;)</h2>
+    <div className="container" id="page-player-status">
+      <PageHeader title="Estas são as suas estatísticas." />
+
+      <main>
+        <p>Vitórias</p>
+        <p>Partidas jogadas</p>
+        <p>Índice de vitórias (vitórias/partidas)</p>
+        <p>Dificuldade mais jogada</p>
+        <p>Últimas 4 contas customizadas</p>
+        <p>Entrou por último</p>
+        <p>Média de duração de uma partida</p>
+        <p>Acertos seguidos</p>
+        <p>Cards revelados</p>
+        <p>Pares formados</p>
+      </main>
     </div>
   );
 }
