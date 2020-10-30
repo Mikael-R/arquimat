d58cb8f0f51560e71965865372fabf1b378d357b
Mikael Rolim de Aquino <mikarg9@gmail.com>
2020-10-30 22:12:41 +0000

    feat: 🎸 add footer in player status page

--- src/pages/PlayerStatus/index.tsx
+++ src/pages/PlayerStatus/index.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react';
 
+import purpleHeartIcon from '../../assets/icons/purple-heart.svg';
 import PageHeader from '../../components/PageHeader';
 
 import './styles.css';
@@ -11,15 +12,33 @@ function PlayerStatus(): ReactElement {
 
       <main>
         <p>Vitórias</p>
+        <hr />
         <p>Partidas jogadas</p>
+        <hr />
         <p>Índice de vitórias (vitórias/partidas)</p>
+        <hr />
         <p>Dificuldade mais jogada</p>
+        <hr />
         <p>Últimas 4 contas customizadas</p>
+        <hr />
         <p>Entrou por último</p>
+        <hr />
         <p>Média de duração de uma partida</p>
+        <hr />
         <p>Acertos seguidos</p>
+        <hr />
         <p>Cards revelados</p>
+        <hr />
         <p>Pares formados</p>
+
+        <footer>
+          <p>
+            Feito com
+            <img src={purpleHeartIcon} alt="Coração roxo" />
+            por
+            <a href="https://mikael-r.github.io/contact/">Mikael</a>
+          </p>
+        </footer>
       </main>
     </div>
   );
