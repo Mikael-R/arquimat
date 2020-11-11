723ec4406209fd041392e90299d12b44a9b7d6d4
Mikael-R <mikarg9@gmail.com>
2020-11-11 14:02:55 -0300

    refactor: 💡 remove unnused file

--- src/tools/convertNodeListOfToArray.ts
+++
@@ -1,7 +0,0 @@
-function convertNodeListOfToArray<T extends Node = any>(
-  nodeListOf: NodeListOf<T>,
-): T[] {
-  return Array.prototype.slice.call(nodeListOf);
-}
-
-export default convertNodeListOfToArray;
